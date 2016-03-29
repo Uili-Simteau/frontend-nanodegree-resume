44708d8311eff8bdafa094e5fa0043356c3e1d22
Uili-Simteau <simteau@gmail.com>
2016-03-29 14:22:14 +1300

    this commit will add name and role to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,10 +2,19 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 // $("#main").append("Uili Fecteau");
- var awesomeThoughts = "I am Uili and I am AWESOME!!";
+ /*var awesomeThoughts = "I am Uili and I am AWESOME!!";
  console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
 console.log(awesomeThoughts);
 console.log(funThoughts);
-$("#main").append(funThoughts);
+$("#main").append(funThoughts); */
+
+var formattedName = HTMLheaderName.replace("%data%", "Uili Fecteau");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
+
+console.log(formattedName);
+console.log(formattedRole);
