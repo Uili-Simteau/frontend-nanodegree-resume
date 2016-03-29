87e081bcf7da44a7149c8942e68ae35cd0e5c339
Uili-Simteau <simteau@gmail.com>
2016-03-30 11:46:05 +1300

    this commit will add work info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,49 +9,94 @@ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(awesomeThoughts);
 console.log(funThoughts);
 $("#main").append(funThoughts); */
-
-var formattedName = HTMLheaderName.replace("%data%", "Uili Fecteau");
-var skills = ["awesomeness", "JavaScript", "Art History"];
-
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
 var bio = {
     "name" : "Uili Fecteau",
     "Role" : "Web Developer",
-    "comtacts" : {
-        "mobile" : "021 *****22",
-        "Email": "simteau@gmail.com",
+    "contacts" : {
+        "mobile" : "021 2394522",
+        "email": "simteau@gmail.com",
         "github" : "Uili-Simteau.github.io.git",
         "twitter" : "Simteau",
         "blog" : "simteau",
         "location" : "Naenae, Lower Hutt, 5011, New Zealand"
 },
-
     "welcomeMsg" : "Hi there, Welcome to my resume! Take your shoes off, stay a while.",
-    "skills" : skills,
+    "skills" : ["Awesomeness", " JavaScript", " HTML", "CSS", " Art History", " Archival Research"],
     "bioPic" : "images/image-uili.jpeg",
 };
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.github);
-var formattedBlog = HTMLblog.replace("%data%", bio.blog);
-var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+//variables for modifying formatted elements with bio data
+var formattedName = HTMLheaderName.replace("%data%", "Uili Fecteau");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
+var formattedBlog = HTMLblog.replace("%data%", bio.contacts["blog"]);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
+
+//append formatted data
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
 $("#topContacts").prepend(formattedMobile);
 $("#topContacts").prepend(formattedEmail);
 $("#topContacts").prepend(formattedTwitter);
 $("#topContacts").prepend(formattedGithub);
 $("#topContacts").prepend(formattedBlog);
 $("#topContacts").prepend(formattedLocation);
-$("#main").prepend(formattedBioPic);
 $("#topContacts").prepend(formattedWelcomeMsg);
 $("#topContacts").append(formattedSkills);
 
+
+//an array of objects
+var work = {
+    "jobs" : [
+    {
+        "position" : "student",
+        "employer" : "Enspiral Dev Academy",
+        "dates" : "2016",
+        "city" : "Wellington",
+        "description" : "Awesome Web Developer adn nice guy in training."
+    },
+
+    {
+        "position" : "Under Tile Heating Installer",
+        "employer" : "Warmup Wellington Ltd.",
+        "dates" : "2014 to 2016",
+        "city" : "Wellington"
+        "description" : "I installed underfloor heating elements, and constructed tiled showers with sheet membraine waterproofing",
+    },
+
+    {
+        "position" : "Archivist",
+        "employer" : "Archives New Zealand",
+        "dates" : "2006 to 2014",
+        "city" : "Wellington"
+        "description" : "Providing archival assistance to researchers, arrangement and description work on the film negative collection. YouTube channel development."
+    },
+
+    {
+        "position" : "Paint consultant and Bunnings Team Member",
+        "employer" : "Bunnings",
+        "dates" : "2004 - 2006",
+        "city" : "Lower Hutt"
+        "description" : ""
+    },
+
+    {
+        "position" : "student",
+        "employer" : "Enspiral Dev Academy",
+        "dates" : "2016",
+        "city" : "Wellington"
+        "description" : ""
+    },
+
+    ]
+};
 
