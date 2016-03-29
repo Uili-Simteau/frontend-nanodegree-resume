deeea80cf0215deaea6087a4d4dfdc81daabdef8
Uili-Simteau <simteau@gmail.com>
2016-03-29 14:00:42 +1300

    this commit will add I am awesome

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Uili Fecteau");
+// $("#main").append("Uili Fecteau");
+ var awesomeThoughts = "I am Uili and I am AWESOME!!";
+ console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+$("#main").append(funThoughts);
