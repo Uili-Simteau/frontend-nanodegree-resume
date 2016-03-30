9a177873c5d5aab876eafaa305201ebaad6671e1
Uili-Simteau <simteau@gmail.com>
2016-03-30 14:42:23 +1300

    this commit will backup cleaned out resume builder file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,4 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-// $("#main").append("Uili Fecteau");
- /*var awesomeThoughts = "I am Uili and I am AWESOME!!";
- console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-console.log(awesomeThoughts);
-console.log(funThoughts);
-$("#main").append(funThoughts); */
+//bio vars
 var bio = {
     "name" : "Uili Fecteau",
     "Role" : "Web Developer",
